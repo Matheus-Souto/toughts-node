6afcedd437e5c8f87e22e8132d789716e4f3d66e
Matheus Souto <emaildopaypal007@gmail.com>
2024-04-26 09:25:28 -0300

    configura projeto

--- index.js
+++ index.js
@@ -8,6 +8,49 @@ const app = express();
 
 const conn = require("./db/conn");
 
+// template egine setup
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+// body response parser
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require("path").join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  })
+);
+
+// flash middleware
+app.use(flash());
+
+//public path
+app.use(express.static("public"));
+
+// set session response
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
 conn
   .sync()
   .then(() => {
