a38676100bdfc2d7d82b0a99596e8e33aab45c62
Matheus Souto <emaildopaypal007@gmail.com>
2024-04-29 00:28:36 -0300

    implementa registro no sistema

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -14,11 +14,47 @@ module.exports = class AuthController {
   static async registerPost(req, res) {
     const { name, email, password, confirmpassword } = req.body;
 
+    // password match validation
     if (password !== confirmpassword) {
       req.flash("message", "As senhas não conferem, tente novamente!");
       res.render("auth/register");
 
       return;
+    }
+
+    // check if user exists
+    const checkifUserExists = await User.findOne({ where: { email } });
+
+    if (checkifUserExists) {
+      req.flash("message", "O e-mail já está cadastrado!");
+      res.render("auth/register");
+
+      return;
+    }
+
+    // create a password hash
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+    };
+
+    try {
+      const createdUser = await User.create(user);
+
+      //initialize session
+      req.session.userid = createdUser.id;
+
+      req.flash("message", "Usuário cadastrado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (err) {
+      console.log(err);
     }
   }
 };
